353c83addd7828ef355bbf79b96e11803a5686f0
Will Sides <32076464+willsides@users.noreply.github.com>
2025-02-20 22:15:07 -0800

    once again

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -64,7 +64,7 @@ if [ -n "$REQUEST_BODY_LIMIT" ]; then
         sed -i "s/^LimitRequestBody .*/LimitRequestBody $REQUEST_BODY_LIMIT/" "$HTTPD_PREFIX/conf/conf-available/dav.conf"
     else
         echo "No existing LimitRequestBody, appending..."
-        echo "LimitRequestBody $REQUEST_BODY_LIMIT" | tee -a "$HTTPD_PREFIX/conf/conf-available/dav.conf" > /dev/null
+        printf "LimitRequestBody $REQUEST_BODY_LIMIT" > "$HTTPD_PREFIX/conf/conf-available/dav.conf"
 
         # Verify immediately after writing
         echo "Verifying the contents of dav.conf after appending:"
